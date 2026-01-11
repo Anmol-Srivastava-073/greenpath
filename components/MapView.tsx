37fc95848191f04283b6f3438b44f2c0292e2b66
ANMOL SRIVASTAVA <anmolsrivastava073@gmail.com>
2026-01-11 19:43:01 +0530

    Update MapView.tsx

--- components/MapView.tsx
+++ components/MapView.tsx
@@ -1,29 +1,40 @@
 "use client";
 
-import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+// Fix Next.js icon issue
+const DefaultIcon = L.icon({
+  iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+});
+L.Marker.prototype.options.icon = DefaultIcon;
 
 export default function MapView({ points }: { points: any[] }) {
-  const { isLoaded } = useJsApiLoader({
-    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY!,
-  });
-
-  if (!isLoaded) return <p>Loading map...</p>;
-
   return (
-    <GoogleMap
-      center={{ lat: 20.5937, lng: 78.9629 }}
-      zoom={5}
-      mapContainerStyle={{ width: "100%", height: "500px" }}
-    >
-      {points.map((p) => (
-        <Marker
-          key={p.id}
-          position={{
-            lat: p.latitude,
-            lng: p.longitude,
-          }}
+    <div style={{ height: "500px", width: "100%", marginTop: "20px" }}>
+      <MapContainer
+        center={[20.5937, 78.9629]} // India center
+        zoom={5}
+        style={{ height: "100%", width: "100%" }}
+      >
+        {/* Free map tiles */}
+        <TileLayer
+          attribution="© OpenStreetMap contributors"
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-      ))}
-    </GoogleMap>
+
+        {/* Dynamic waste markers */}
+        {points.map((p) => (
+          <Marker key={p.id} position={[p.latitude, p.longitude]}>
+            <Popup>
+              <strong>Waste Type:</strong> {p.waste_type} <br />
+              <strong>Bin:</strong> {p.bin_type}
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+    </div>
   );
 }
